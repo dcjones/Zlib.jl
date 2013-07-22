56c8f9779265fe27292c8d94616a30c024a294d5
Fazlul Shahriar <fshahriar@gmail.com>
2013-07-22 04:06:11 -0400

    add support for raw deflate (without zlib header)

--- test/test.jl
+++ test/test.jl
@@ -5,5 +5,8 @@ data = convert(Vector{Uint8}, rand(1:255, 1000000))
 decompressed = decompress(compress(data))
 @assert data == decompressed
 
+decompressed = decompress(compress(data, false, true), true)
+@assert data == decompressed
+
 @assert length(decompress(compress(""))) == 0
 
